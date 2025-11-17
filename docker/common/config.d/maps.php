304b043d20d24274518dcea45a8360df753e4197
sphtere <sph.tere@gmail.com>
2025-11-17 17:58:04 -0300

    Posição inical do mapa alterado para a cidade de Teresópolis

--- docker/common/config.d/maps.php
+++ docker/common/config.d/maps.php
@@ -2,11 +2,11 @@
 
 return [
     'maps.center'  => [
-        -14.2400732, //latitude
-        -53.1805018 // longitude
+        -22.33357, //latitude
+        -42.94085  //longitude
     ],
     
-    'maps.zoom.default'     => 5,
+    'maps.zoom.default'     => 12,
     'maps.zoom.approximate' => 14,
     'maps.zoom.precise'     => 16,
     'maps.zoom.max'         => 18,
